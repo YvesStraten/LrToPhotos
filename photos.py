78a3ea3c7539d0aec621da33b8676a85f1186575
YvesStraten <yves.straten@gmail.com>
2024-04-20 17:02:16 +0800

    Link with lightroom

--- photos.py
+++ photos.py
@@ -1,23 +1,69 @@
 #!/usr/bin/env python3
 
 import os
-from osxphotos.cli.batch_edit import photoscript_photo
+import sys
 import osxphotos
+from osxphotos.cli.batch_edit import photoscript_photo
+
+from lrtools.lrcat import LRCatDB, LRCatException
+from lrtools.lrselectgeneric import LRSelectException
+from lrtools.display import display_results
+
 
 def main() -> None:
-    path = os.path.expanduser("~/Pictures/Test.photoslibrary")
-    photos = osxphotos.PhotosDB(path)
+    try:
+        path = os.path.expanduser("~/Downloads/Lightroom Catalog.lrcat")
+        lrdb = LRCatDB(path)
+    except LRCatException:
+        print("Could not access")
 
-    for photo in photos.photos():
-        editable = photoscript_photo(photo)
-        editable.keywords = [ "nlah", "hello" ]
+    columns = "name,keywords"
 
-    # Remove the object, and refresh db
-    del photos
+    try:
+        rows = lrdb.lrphoto.select_generic(columns).fetchall()
 
-    photos = osxphotos.PhotosDB(path)
+    except LRSelectException:
+        print("No photo")
 
-    for photo in photos.photos():
-        print(photo.keywords)
+
+
+    counter = 0
+    photosWithKeywords = [  ]
+
+    for row in rows:
+
+        if row[1] != None:
+            photosWithKeywords.append([row[0], row[1]])
+
+
+            counter += 1
+
+    print(counter)
+
+    sort = sorted(photosWithKeywords, key=lambda field: field[1])
+    # for photo in photosWithKeywords:
+    #     print("filename:", photo[0], "Tags: ", photo[1])
+    #     # for index, field in enumerate(row):
+    #     #     print(index)
+    #     #     print(field)
+    #     #
+    for field in sort:
+        for index, _ in enumerate(field):
+            print("filename:", field[0], "Tags: ", field[1])
+
+    # path = os.path.expanduser("~/Pictures/Test.photoslibrary")
+    # photos = osxphotos.PhotosDB(path)
+
+    # for photo in photos.photos():
+    #     editable = photoscript_photo(photo)
+    #     editable.keywords = [ "nlah", "hello" ]
+
+    # # Remove the object, and refresh db
+    # del photos
+
+    # photos = osxphotos.PhotosDB(path)
+
+    # for photo in photos.photos():
+    #     print(photo.keywords)
 
 main()

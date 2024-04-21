13d80520fa1e77dde68790d1fb770fb16db369ed
YvesStraten <yves.straten@gmail.com>
2024-04-21 10:26:43 +0800

    Added args checking

--- photosDriver.py
+++ photosDriver.py
@@ -3,7 +3,6 @@
 import os
 import sys
 import osxphotos
-from osxphotos import photokit
 from osxphotos.cli.batch_edit import photoscript_photo
 
 from lrtools.lrcat import LRCatDB, LRCatException
@@ -11,19 +10,16 @@ from lrtools.lrselectgeneric import LRSelectException
 
 from photos import PhotoEntity, PhotoList
 
-
-def main() -> None:
+def transferKeywords(applePath: str, LightRoomPath:str) -> None:
     try:
-        path = os.path.expanduser("~/Pictures/Test.photoslibrary")
-        photos = osxphotos.PhotosDB(path)
+        photos = osxphotos.PhotosDB(applePath)
     except:
         print("Could not open photos database")
 
     try:
         columns = "name,keywords"
 
-        path = os.path.expanduser("~/Downloads/Lightroom Catalog.lrcat")
-        lrdb = LRCatDB(path)
+        lrdb = LRCatDB(LightRoomPath)
         rows = lrdb.lrphoto.select_generic(columns).fetchall()
 
     except LRCatException:
@@ -51,12 +47,7 @@ def main() -> None:
     for photo in photosWithKeywords:
         print(photo)
 
-    print("Number of photos:", len(photosWithKeywords))
-    print("Apple db")
-
-    for photo in photos.photos():
-        print(photo.filename)
-        print(photo)
+    print("Number of photos to transfer:", len(photosWithKeywords))
 
     # for photo in photos.photos():
     #     editable = photoscript_photo(photo)
@@ -70,5 +61,33 @@ def main() -> None:
     # for photo in photos.photos():
     #     print(photo.keywords)
 
+
+def main() -> None:
+    args = sys.argv
+
+    helpMenu = (
+        "Photos Keyword transferer \n",
+        "Usage: python photosDriver.py <path to .photoslibrary> <path to .lrcat>",
+        "All paths are relative, e.g ~/User/Downloads"
+    )
+
+    if len(args) > 3 or len(args) == 1:
+        for item in helpMenu:
+            print(item)
+
+    else:
+        try:
+            photosPath = os.path.expanduser(sys.argv[1])
+            lrcatPath = os.path.expanduser(sys.argv[2])
+
+            if not os.path.exists(photosPath) or not os.path.exists(lrcatPath):
+                raise Exception()
+            else:
+                transferKeywords(photosPath, lrcatPath)
+
+        except:
+            print("Wrong paths to catalogs \n")
+            for item in helpMenu:
+                print(item)
 
 main()
